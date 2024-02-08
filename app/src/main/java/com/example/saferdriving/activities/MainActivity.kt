89217362f0f6de2c4c4cdde8999e2f7fc18fb3b4
Max <maxt@itu.dk>
2024-02-08 12:30:56 +0100

    Test in MainActivity

--- app/src/main/java/com/example/saferdriving/activities/MainActivity.kt
+++ app/src/main/java/com/example/saferdriving/activities/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.example.saferdriving.activities
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.saferdriving.R
 import com.example.saferdriving.databinding.ActivityMainBinding
+import com.example.saferdriving.enums.ObdTypes
 import com.github.eltonvs.obd.command.engine.SpeedCommand
-import com.github.eltonvs.obd.connection.ObdDeviceConnection
-import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.io.IOException
-import java.net.InetAddress
-import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -25,5 +21,25 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val obdConnection =
+                    ObdTypes.BLUETOOTH.connect(this@MainActivity, "11:22:33:44:55:66")
+
+                val response = obdConnection.run(SpeedCommand()).value
+                launch(Dispatchers.Main) {
+                    binding.outputText.text = getString(R.string.speed_result, response)
+                }
+
+            } catch (e: IOException) {
+                e.printStackTrace() // Print the exception trace to identify the issue
+                launch(Dispatchers.Main) {
+                    binding.outputText.text = getString(R.string.speed_result, e.message)
+                }
+            }
+        }
     }
 }
