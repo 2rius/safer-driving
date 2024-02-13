874110e4bb049b6f885a046f38be2da4fe0a7e33
Mads0709 <112487518+Mads0709@users.noreply.github.com>
2024-02-13 13:38:47 +0100

    layout and actvities

---
+++ app/src/main/java/com/example/saferdriving/activities/RegisterDriver.kt
@@ -0,0 +1,12 @@
+package com.example.saferdriving.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.saferdriving.R
+
+class RegisterDriver : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_driver)
+    }
+}
