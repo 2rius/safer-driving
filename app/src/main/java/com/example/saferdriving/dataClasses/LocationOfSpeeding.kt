0a96b3a077e3cbbfbff0cee86f46a0d518347175
Mads0709 <112487518+Mads0709@users.noreply.github.com>
2024-02-13 13:00:03 +0100

    add dataclasses

---
+++ app/src/main/java/com/example/saferdriving/dataClasses/LocationOfSpeeding.kt
@@ -0,0 +1,8 @@
+package com.example.saferdriving.dataClasses
+
+data class LocationOfSpeeding(
+    var id: Int? = null,
+    var lat: Double? = null,
+    var long: Double? = null,
+    var topSpeed: Int? = null
+)
